2002332829796e48a61e8298027eae8f77f7803f
Russell Purinton <russell.purinton@gmail.com>
2023-11-29 11:09:10 -0500

    Update new_listings.php

--- scripts/new_listings.php
+++ scripts/new_listings.php
@@ -15,6 +15,11 @@ ini_set('display_errors', '1');
 try {
     require_once __DIR__ . "/../Composer.php";
     $log = LogFactory::create("new_listings-$worker_id") or throw new Error("failed to create log");
+    set_exception_handler(function ($e) use ($log) {
+        $log->debug($e->getMessage(), ["trace" => $e->getTrace()]);
+        $log->error($e->getMessage());
+        exit(1);
+    });
 } catch (\Exception $e) {
     echo ("Fatal Exception " . $e->getMessage() . "\n");
     exit(1);
@@ -25,26 +30,6 @@ try {
     echo ("Fatal Error " . $e->getMessage() . "\n");
     exit(1);
 }
-
-try {
-    set_exception_handler('global_exception_handler');
-    $me = new NewListingsConsumer($log, new MySQL($log), new RabbitConsumer()) or throw new Error("failed to create NewListingsConsumer");
-    $me->run() or throw new Error("failed to run NewListingsConsumer");
-    $log->info("NewListingsConsumer running...");
-} catch (\Exception $e) {
-    $log->debug("Fatal Exception " . $e->getMessage(), ["trace" => $e->getTrace()]);
-    $log->error("Fatal Exception " . $e->getMessage());
-} catch (\Throwable $e) {
-    $log->debug("Fatal Throwable " . $e->getMessage(), ["trace" => $e->getTrace()]);
-    $log->error("Fatal Throwable " . $e->getMessage());
-} catch (\Error $e) {
-    $log->debug("Fatal Error " . $e->getMessage(), ["trace" => $e->getTrace()]);
-    $log->error("Fatal Error " . $e->getMessage());
-}
-
-function global_exception_handler($e)
-{
-    global $log;
-    $log->debug("Fatal Exception " . $e->getMessage(), ["trace" => $e->getTrace()]);
-    $log->error("Fatal Exception " . $e->getMessage());
-}
+$me = new NewListingsConsumer($log, new MySQL($log), new RabbitConsumer()) or throw new Error("failed to create NewListingsConsumer");
+$me->run() or throw new Error("failed to run NewListingsConsumer");
+$log->info("NewListingsConsumer running...");
