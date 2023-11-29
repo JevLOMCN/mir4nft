ef7425e6220ac5ca6a95bbf2b219d882ffe090bc
Russell Purinton <russell.purinton@gmail.com>
2023-11-29 10:51:28 -0500

    Update RabbitMQ.php

--- src/RabbitMQ.php
+++ src/RabbitMQ.php
@@ -22,29 +22,22 @@ class RabbitMQ
         $this->client = Async\await($this->client->connect()) or throw new Error('Failed to establish the connection');
         $this->channel = Async\await($this->client->channel()) or throw new Error('Failed to establish the channel');
         Async\await($this->channel->qos(0, 1)) or throw new Error('Failed to set the QoS');
-        Async\await($this->channel->queueDeclare($this->queue)) or throw new Error('Failed to declare the queue');
         return Async\await($this->channel->consume($process, $this->queue, $this->consumerTag, false, true)) or throw new Error('Failed to consume the queue');
     }
 
     public function publish(string $queue, array $data): bool
     {
+        $result = false;
         try {
-            $client = new Client(Loop::get(), Config::get("rabbitmq")) or throw new Error('Failed to establish the client');
-            $client = Async\await($client->connect()) or throw new Error('Failed to establish the connection');
-            $channel = Async\await($client->channel()) or throw new Error('Failed to establish the channel');
-            Async\await($channel->queueDeclare($queue)) or throw new Error('Failed to declare the queue');
-            Async\await($channel->publish(json_encode($data), [], '', $queue)) or throw new Error('Failed to publish the message');
+            $result = Async\await($this->channel->publish(json_encode($data), [], '', $queue)) or throw new Error('Failed to publish the message');
         } catch (\Throwable $e) {
             print_r($e);
         } catch (\Error $e) {
             print_r($e);
         } catch (\Exception $e) {
             print_r($e);
-        } finally {
-            if (isset($channel) && $channel) $channel->close();
-            if (isset($client) && $client) $client->disconnect();
         }
-        return true;
+        return $result;
     }
 
     public function disconnect(): bool
