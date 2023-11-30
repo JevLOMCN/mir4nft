0077c2e777b5dfca6c450630fc4583a1c926d8d1
Russell Purinton <russell.purinton@gmail.com>
2023-11-30 13:49:58 -0500

    Update HTTPS.php

--- src/HTTPS.php
+++ src/HTTPS.php
@@ -4,27 +4,26 @@ namespace RPurinton\Mir4nft;
 
 class HTTPS
 {
-    static function get($url): string
+    static function get(string $url, array $headers = []): string
     {
+        $headers[] = "User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.4577.82 Safari/537.36";
         $response = file_get_contents($url, false, stream_context_create([
             'http' => [
                 'method' => 'GET',
-                'header' => "User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) " .
-                    "AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.4577.82 Safari/537.36\r\n"
+                'header' => implode("\r\n", $headers)
             ]
         ])) or throw new Error("failed to get contents");
         return $response;
     }
 
-    static function post($url, $data): string
+    static function post(string $url, array $headers = [], string $body = ""): string
     {
+        $headers[] = "User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.4577.82 Safari/537.36";
         $response = file_get_contents($url, false, stream_context_create([
             'http' => [
                 'method' => 'POST',
-                'header' => "User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) " .
-                    "AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.4577.82 Safari/537.36\r\n" .
-                    "Content-Type: application/json\r\n",
-                'content' => json_encode($data)
+                'header' => implode("\r\n", $headers),
+                'content' => $body
             ]
         ])) or throw new Error("failed to get contents");
         return $response;
