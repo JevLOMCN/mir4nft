5af2617dfce80db20ad6932584a6dc6205ec601b
Russell Purinton <russell.purinton@gmail.com>
2023-11-30 08:01:06 -0500

    Update HTTPS.php

--- src/HTTPS.php
+++ src/HTTPS.php
@@ -6,12 +6,15 @@ class HTTPS
 {
     static function get($url): string
     {
-        return file_get_contents($url, false, stream_context_create([
+        echo ("getting $url\n");
+        $response = file_get_contents($url, false, stream_context_create([
             'http' => [
                 'method' => 'GET',
                 'header' => "User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) " .
                     "AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.4577.82 Safari/537.36\r\n"
             ]
         ])) or throw new Error("failed to get contents");
+        echo ("got $response\n");
+        return $response;
     }
 }
