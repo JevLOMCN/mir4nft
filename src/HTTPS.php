041d77d974129d763931ec8c39ad9c7ebf9b18a1
Russell Purinton <russell.purinton@gmail.com>
2023-11-30 13:41:09 -0500

    Update HTTPS.php

--- src/HTTPS.php
+++ src/HTTPS.php
@@ -15,4 +15,18 @@ class HTTPS
         ])) or throw new Error("failed to get contents");
         return $response;
     }
+
+    static function post($url, $data): string
+    {
+        $response = file_get_contents($url, false, stream_context_create([
+            'http' => [
+                'method' => 'POST',
+                'header' => "User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) " .
+                    "AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.4577.82 Safari/537.36\r\n" .
+                    "Content-Type: application/json\r\n",
+                'content' => json_encode($data)
+            ]
+        ])) or throw new Error("failed to get contents");
+        return $response;
+    }
 }
