a9b1fd7719aaac8688a2b9f5ee857fbaa848929e
Russell Purinton <russell.purinton@gmail.com>
2023-12-01 21:05:28 -0500

    cleanup

--- scripts/export/upload.php
+++
@@ -1,7 +0,0 @@
-<?php
-$file_ids = [];
-$cli = "curl https://api.openai.com/v1/files   -H \"Authorization: Bearer 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\"   -F \"purpose=fine-tune\"   -F \"file=@data.jsonl\"";
-$json_string = shell_exec($cli);
-$json = json_decode($json_string, true);
-$file_ids[] = $json["id"];
-file_put_contents("file_ids.json", json_encode($file_ids, JSON_PRETTY_PRINT));
