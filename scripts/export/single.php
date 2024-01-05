23c5ed006a9a0949f554ff43e2b67c869c3117d5
Laozi 老子 <97992269+rpurinton@users.noreply.github.com>
2024-01-05 04:08:31 -0500

    Update single.php

--- scripts/export/single.php
+++ scripts/export/single.php
@@ -6,40 +6,11 @@ use COM;
 use RPurinton\Mir4nft\OpenAI\Client;
 
 
-function getGrade($grade)
-{
-    $grade = strval($grade);
-    return match ($grade) {
-        "1" => "1",
-        "2" => "2",
-        "3" => "3",
-        "4" => "4",
-        "5" => "5",
-        default => "1"
-    };
-}
+require_once(__DIR__ . '/../../Composer.php');
+$log = LogFactory::create("single");
+$sql = new MySQL($log);
+$ai = new Client($log);
 
-function getClass($class)
-{
-    $class = strval($class);
-    return match ($class) {
-        "1" => "Warrior",
-        "2" => "Sorcerer",
-        "3" => "Taoist",
-        "4" => "Arbalist",
-        "5" => "Lancer",
-        "6" => "Darkist",
-        default => "Warrior"
-    };
-}
-
-function tradeable($itemID)
-{
-    return match (substr($itemID, 3, 1)) {
-        "1" => "yes",
-        default => "no"
-    };
-}
 
 if (!isset($argv[1])) {
     echo "Usage: php single.php <seq>\n";
@@ -48,12 +19,8 @@ if (!isset($argv[1])) {
 
 $seq = $argv[1];
 
-// Database connection
-$db = mysqli_connect("127.0.0.1", "mir4nft", "mir4nft", "mir4nft");
-
 // Fetch all transportID and class from the db
-$result = $db->query("SELECT `sequence`.`usd_price`,
-        `summary`.`json` AS `summary`,
+$result = $sql->query("SELECT `summary`.`json` AS `summary`,
         `assets`.`json` AS `assets`,
         `building`.`json` AS `building`,
         `codex`.`json` AS `codex`,
@@ -79,106 +46,13 @@ $result = $db->query("SELECT `sequence`.`usd_price`,
     INNER JOIN `stats` ON `sequence`.`transportID` = `stats`.`transportID`
     INNER JOIN `training` ON `sequence`.`transportID` = `training`.`transportID`
     WHERE `sequence`.`seq` = $seq");
+
 if (!$result) {
     echo "Error: " . $db->error . "\n";
     exit;
 }
-$row = $result->fetch_assoc();
-$record = [];
 
-// summary
-$summary = json_decode($row['summary'], true)['data'];
-// transport parts
-$record['class'] = getClass($summary['character']['class']);
-$record['level'] = $summary['character']['level'];
-$record['powerScore'] = $summary['character']['powerScore'];
-foreach ($summary['equipItem'] as $equipItem) $record['equipItems'][] = [
-    'name' => $equipItem['itemName'],
-    'grade' => getGrade($equipItem['grade']),
-    'tier' => $equipItem['tier'],
-    'enhance' => $equipItem['enhance'],
-];
-
-// assets
-$record['assets'] = json_decode($row['assets'], true)['data'];
-
-// building
-$buildings = json_decode($row['building'], true)['data'];
-foreach ($buildings as $building) {
-    $buildingName = $building['buildingName'];
-    $record['conquests'][$buildingName] = $building['buildingLevel'];
-}
-
-// codex
-$codex = json_decode($row['codex'], true)['data'];
-foreach ($codex as $codexItem) {
-    $codexName = $codexItem['codexName'];
-    unset($codexItem['codexName']);
-    $record['codex'][$codexName] = $codexItem;
-}
-
-// holystuff
-$holystuff = json_decode($row['holystuff'], true)['data'];
-foreach ($holystuff as $holystuffItem) {
-    $holystuffName = $holystuffItem['HolyStuffName'];
-    $record['holystuff'][$holystuffName] = $holystuffItem['Grade'];
-}
-
-// magicorb
-$magicorb = json_decode($row['magicorb'], true)['data']['equipItem'];
-foreach ($magicorb as $deck) foreach ($deck as $item) $record['magicorbs'][$item['itemName']] = [
-    'grade' => getGrade($item['grade']),
-    'level' => $item['itemLv'],
-    'exp' => $item['itemExp'],
-    'tier' => $item['tier'],
-];
-
-// mysticalpiece
-$mysticalpiece = json_decode($row['mysticalpiece'], true)['data']['equipItem'];
-foreach ($mysticalpiece as $deck) foreach ($deck as $item) {
-    if ($item['grade'] >= 4) $record['mysticalpieces'][$item['itemName']] = [
-        'grade' => getGrade($item['grade']),
-        'tier' => $item['tier']
-    ];
-}
-
-// potential
-$record['potential'] = json_decode($row['potential'], true)['data'];
-
-// skills
-$skills = json_decode($row['skills'], true)['data'];
-foreach ($skills as $skill) {
-    $record['skills'][$skill['skillName']] = $skill['skillLevel'];
-}
-
-// spirit
-$spirit = json_decode($row['spirit'], true)['data']['inven'];
-foreach ($spirit as $spiritItem) {
-    if ($spiritItem['grade'] >= 4) $record['spirits'][] = [
-        'name' => $spiritItem['petName'],
-        'grade' => getGrade($spiritItem['grade'])
-    ];
-}
-
-// stats
-$stats = json_decode($row['stats'], true)['data']['lists'];
-$stats_wanted = ["HP", "MP", "PHYS ATK", "PHYS DEF", "Spell ATK", "Spell DEF", "Accuracy", "EVA", "CRIT", "CRIT EVA"];
-foreach ($stats as $stat) if (in_array($stat['statName'], $stats_wanted)) $record['stats'][$stat['statName']] = $stat['statValue'];
-
-
-// training
-$training = json_decode($row['training'], true)['data'];
-$record['training']['Constitution'] = $training['consitutionLevel'] ?? "Unknown";
-$record['training']['Solitude'] = $training['collectLevel'] ?? "Unknown";
-unset($training['consitutionLevel']);
-unset($training['collectLevel']);
-unset($training['consitutionName']);
-unset($training['collectName']);
-foreach ($training as $trainingItem) $record['training'][$trainingItem['forceName']] = $trainingItem['forceLevel'];
-
-
-require_once(__DIR__ . '/../../Composer.php');
-$log = LogFactory::create("single");
-$ai = new Client($log);
-$usd = json_decode($ai->complete(json_encode($record)), true)['usd'];
-echo "USD: \$$usd\n";
+$record = Export::row($result->fetch_assoc());
+echo (json_encode($record, JSON_PRETTY_PRINT) . "\n");
+$usd_price = json_decode($ai->complete(json_encode($record)), true)['usd_price'];
+echo "USD Price: \$$usd_price\n";
