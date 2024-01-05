90cf71d5e08019b27b49fca12af7ff74dc9e425d
Laozi 老子 <97992269+rpurinton@users.noreply.github.com>
2024-01-04 20:49:01 -0500

    Update Config.php

--- src/Config.php
+++ src/Config.php
@@ -4,7 +4,7 @@ namespace RPurinton\Mir4nft;
 
 class Config
 {
-    public static function get(string $file): mixed
+    public static function get(string $file): array|bool|string|int|float
     {
         $config = json_decode(file_get_contents(__DIR__ . "/../config/$file.json"), true);
         if (!$config) throw new Error("Failed to load config file: $file.json");
