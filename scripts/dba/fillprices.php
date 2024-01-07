aee1450d1b9ddd6afc3b4775a8302a6f6f2f692f
Laozi 老子 <97992269+rpurinton@users.noreply.github.com>
2024-01-07 00:44:42 -0500

    Update fillprices.php

--- scripts/dba/fillprices.php
+++ scripts/dba/fillprices.php
@@ -15,4 +15,18 @@ $log = LogFactory::create('fillprices');
 $sql = new MySQL($log);
 $ai = new Client($log);
 
-$log->info("Starting fillprices.php");
+$query = "SELECT `transportID`
+FROM `transports`
+WHERE `transportID` NOT IN (
+  SELECT DISTINCT `transportID`
+  FROM `evals`
+)";
+$result = $sql->query($query);
+$total = $result->num_rows;
+$counter = 0;
+while ($row = $result->fetch_assoc()) {
+    $counter++;
+    $transportID = $row['transportID'];
+    echo ("\rProcessing $transportID - $counter of $total...");
+}
+echo ("done!\n");
