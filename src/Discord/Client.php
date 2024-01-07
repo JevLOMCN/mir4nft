7a1bdbf415cc7707f4d9e5080013d4025247e233
Laozi 老子 <97992269+rpurinton@users.noreply.github.com>
2024-01-07 18:03:02 -0500

    Discord Stuff

--- src/Discord/Client.php
+++ src/Discord/Client.php
@@ -2,25 +2,6 @@
 
 namespace RPurinton\Mir4nft\Discord;
 
-use Discord\Discord;
-use Discord\Parts\Channel\Message;
-
 class Client
 {
-    public Discord $discord;
-
-    public function __construct()
-    {
-        $this->discord = new Discord([
-            'token' => config('discord.token'),
-        ]);
-    }
-
-    public function sendMessage(string $message): Message
-    {
-        return $this->discord->channel->createMessage([
-            'channel.id' => config('discord.channel_id'),
-            'content' => $message,
-        ]);
-    }
 }
---
+++ src/Discord/Webhook.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace RPurinton\Mir4nft\Discord;
+
+use RPurinton\Mir4nft\{
+    HTTPS,
+    Error,
+};
+
+class Webhook
+{
+    private string $url;
+
+    public function __construct(array $message)
+    {
+        $this->url = getenv('DISCORD_WEBHOOK') or throw new Error("failed to getenv(DISCORD_WEBHOOK)");
+        HTTPS::post($this->url, ['Content-Type: application/json'], json_encode($message)) or throw new Error("failed to post to discord webhook");
+    }
+}
