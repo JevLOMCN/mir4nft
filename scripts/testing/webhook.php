fb5369e80657ec1335a799ed42a8feb972517a2d
Laozi 老子 <97992269+rpurinton@users.noreply.github.com>
2024-01-07 18:10:30 -0500

    Update webhook.php

--- scripts/testing/webhook.php
+++ scripts/testing/webhook.php
@@ -4,4 +4,47 @@ namespace RPurinton\Mir4nft\Discord;
 
 require_once(__DIR__ . '/../../Composer.php');
 
-new Webhook(['content' => 'test']);
+
+$embed = [
+    'title' => 'Title of the Embed',
+    'description' => 'Description of the Embed',
+    'url' => 'https://example.com',
+    'timestamp' => date('Y-m-d\TH:i:s.vZ'),
+    'color' => hexdec('ff0000'),
+    'footer' => [
+        'text' => 'Footer Text',
+        'icon_url' => 'https://example.com/footer-icon.png'
+    ],
+    'image' => [
+        'url' => 'https://example.com/image.png'
+    ],
+    'thumbnail' => [
+        'url' => 'https://example.com/thumbnail.png'
+    ],
+    'video' => [
+        'url' => 'https://example.com/video.mp4'
+    ],
+    'provider' => [
+        'name' => 'Provider Name',
+        'url' => 'https://example.com/provider'
+    ],
+    'author' => [
+        'name' => 'Author Name',
+        'url' => 'https://example.com/author',
+        'icon_url' => 'https://example.com/author-icon.png'
+    ],
+    'fields' => [
+        [
+            'name' => 'Field 1',
+            'value' => 'Value 1',
+            'inline' => false
+        ],
+        [
+            'name' => 'Field 2',
+            'value' => 'Value 2',
+            'inline' => true
+        ]
+    ]
+];
+
+new Webhook(['embeds' => [$embed]]);
