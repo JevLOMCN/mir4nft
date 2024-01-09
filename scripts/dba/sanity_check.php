ca58a39f818512e48a81daef2c28aa703c12c36b
Russell Purinton <russell.purinton@gmail.com>
2024-01-09 01:25:48 -0500

    Update sanity_check.php

--- scripts/dba/sanity_check.php
+++ scripts/dba/sanity_check.php
@@ -9,7 +9,7 @@ while ($row = $result->fetch_assoc()) {
 	$seq = $row['seq'];
 	$transportID = $row['transportID'];
 	echo ("\rRow $counter of $count - $seq...");
-	$transport_check = $sql->query("SELECT * FROM `transport` WHERE `transportID` = '$transportID'");
+	$transport_check = $sql->query("SELECT * FROM `transports` WHERE `transportID` = '$transportID'");
 	if ($transport_check->num_rows == 0) die("\nTransport $seq - $transportID not found");
 }
 echo ("\ndone $counter\n");
