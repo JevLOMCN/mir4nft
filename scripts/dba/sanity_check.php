937adb3aed536583129f19e8b82a82fd8ab1fb78
Russell Purinton <russell.purinton@gmail.com>
2024-01-09 01:25:22 -0500

    Update sanity_check.php

--- scripts/dba/sanity_check.php
+++ scripts/dba/sanity_check.php
@@ -1,14 +1,15 @@
 <?php
 
-$sql = mysqli_connect("127.0.0.1","mir4nft","mir4nft","mir4nft");
+$sql = mysqli_connect("127.0.0.1", "mir4nft", "mir4nft", "mir4nft");
 $result = $sql->query("SELECT * FROM `sequence`");
 $count = $result->num_rows;
 $counter = 0;
-while($row = $result->fetch_assoc())
-{
+while ($row = $result->fetch_assoc()) {
 	$counter++;
 	$seq = $row['seq'];
-	echo("\rRow $counter of $count - $seq...");
+	$transportID = $row['transportID'];
+	echo ("\rRow $counter of $count - $seq...");
+	$transport_check = $sql->query("SELECT * FROM `transport` WHERE `transportID` = '$transportID'");
+	if ($transport_check->num_rows == 0) die("\nTransport $seq - $transportID not found");
 }
-echo("\ndone $counter\n");
-
+echo ("\ndone $counter\n");
